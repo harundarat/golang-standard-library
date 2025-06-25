4afc1674cd1d6fdf45674da65c66a4c0390ff857
harundarat <harundarat@gmail.com>
2025-06-25 21:37:03 +0700

    math

---
+++ math.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(math.Ceil(1.24))
+	fmt.Println(math.Floor(1.99))
+	fmt.Println(math.Round(1.50))
+	fmt.Println(math.Max(2, 9))
+	fmt.Println(math.Min(2, 9))
+}
